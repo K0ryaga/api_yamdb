4fe1591465eafa24b16b6671c7cb6798a10ef765
Антон Стыврин <antonstyvrin@gmail.com>
2023-12-27 19:17:05 +0700

    completed yamdb api

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status, viewsets,  mixins, filters
+from rest_framework import status, viewsets, mixins, filters
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import action, api_view
